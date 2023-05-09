d4d2f2c27a95da8730fe4e0707c019aef762396e
Terry Fu <terryfu33@gmail.com>
2023-05-08 20:18:03 -0600

    fixed working directory

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,8 @@
-import * as vscode from 'vscode';
+/* import * as vscode from 'vscode';
 import * as querystring from 'querystring';
 
 export function activate(context: vscode.ExtensionContext) {
+    
     let disposable = vscode.commands.registerCommand('extension.searchSelectedText', () => {
         let editor = vscode.window.activeTextEditor;
         if (!editor) {
@@ -17,4 +18,61 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(disposable);
+}  */
+ 
+const vscode = require('vscode');
+const { exec } = require('child_process');
+
+function activate(context: { subscriptions: any[]; }) {
+  let disposable = vscode.commands.registerCommand('extension.searchSelectedText', function () {
+    let selectedText;
+    const activeEditor = vscode.window.activeTextEditor;
+    if (activeEditor) {
+      selectedText = activeEditor.document.getText(activeEditor.selection);
+      vscode.window.showInformationMessage('Info Notification2');
+    } else {
+      const activeTerminal = vscode.window.activeTerminal;
+      if (activeTerminal) {
+        //selectedText = activeTerminal.selection;
+        vscode.window.showInformationMessage('Info Notification');
+      }
+    }
+    if (selectedText) {
+      vscode.env.clipboard.writeText(selectedText);
+      const encodedText = encodeURIComponent(selectedText);
+      const url = `https://www.google.com/search?q=${encodedText}`;
+    
+      vscode.window.showInformationMessage(url);
+
+      if (process.platform === "win32") {
+        exec(`start "" "${url}"`);
+      } else if (process.platform === "darwin") {
+        exec(`open "${url}"`);
+      } else {
+        exec(`xdg-open "${url}"`);
+      }
+    }
+  });
+
+  context.subscriptions.push(disposable);
+
+  const terminal = vscode.window.activeTerminal;
+  if (terminal) {
+    context.subscriptions.push(terminal.onDidChangeSelection((e: { text: any; }) => {
+      const selectedText = e.text;
+      if (selectedText) {
+        vscode.env.clipboard.writeText(selectedText);
+        const encodedText = encodeURIComponent(selectedText);
+        const url = `https://www.google.com/search?q=${encodedText}`;
+        vscode.window.showInformationMessage('Info Notification');
+      }
+    }));
+  }
 }
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+};
