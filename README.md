93cb7638b59a19074c6f0aef402e26ff1e8fec96
Terry Fu <terryfu33@gmail.com>
2023-05-12 20:41:11 -0600

    added functionality for terminal

--- README.md
+++ README.md
@@ -8,13 +8,18 @@ https://github.com/terryfu2/ctrl-web/assets/40301450/d8f9ee54-7001-4e6b-bd11-dd7
 </p>
 <!-- markdownlint-enable -->
 
+## Usage
+
+* Select Text in Terminal, 'Ctrl-Shift-c' to open google search 
+* Select Text in Code Editor, 'Ctrl-Alt-c' to open google search 
+
 ## Features
 
-* Adds shortcut 'Ctrl-Shift-c' to Vscode
+* Adds shortcuts 'Ctrl-Shift-c' and 'Ctrl-Alt-c' to Vscode
 * Opens your selected text in a google search of default browser
 * Saves the Alt-Tab -> Ctrl-V workflow
+* Work in terminal(for quick error googling), main code window and output window
 
 ## Coming Soon
 
-* Support in the terminal
 * Other keybinds to simplfy workflow
--- src/extension.ts
+++ src/extension.ts
@@ -1,71 +1,36 @@
 import * as vscode from 'vscode';
 //const vscode = require('vscode');
 const { exec } = require('child_process');
-let myStatusBarItem: vscode.StatusBarItem;
+
+const URL = 'https://www.google.com/search?q=';
 
 function activate({ subscriptions }: vscode.ExtensionContext) {
 
-  const myCommandId = 'extension.searchSelectedText';
-  
-  let disposable = vscode.commands.registerCommand(myCommandId, async function () {
-    let selectedText;
-    const activeEditor = vscode.window.activeTextEditor;
+	vscode.workspace.getConfiguration().update(
+		'terminal.integrated.copyOnSelection',
+		true,
+		vscode.ConfigurationTarget.Global
+	);
 
-    if (activeEditor) {
-      selectedText = activeEditor.document.getText(activeEditor.selection);
-      //vscode.window.showInformationMessage('Info Notification2');
-    } else {
-      const activeTerminal = vscode.window.activeTerminal;
-      if (activeTerminal) {
-        //selectedText = activeTerminal.selection;
-        //vscode.window.showInformationMessage('Info Notification');
-      }
-    }
-    if (selectedText) {
-      vscode.env.clipboard.writeText(selectedText);
-      const encodedText = encodeURIComponent(selectedText);
-      const url = `https://www.google.com/search?q=${encodedText}`;
-    
-      if (process.platform === "win32") {
-        exec(`start "" "${url}"`);
-      } else if (process.platform === "darwin") {
-        exec(`open "${url}"`);
-      } else {
-        exec(`xdg-open "${url}"`);
-      }
-    }
-    /* let selectedText = await vscode.env.clipboard.readText();
-    const encodedText = encodeURIComponent(selectedText);
-    const url = `https://www.google.com/search?q=${encodedText}`;
-    if (process.platform === "win32") {
-      exec(`start "" "${url}"`);
-    } else if (process.platform === "darwin") {
-      exec(`open "${url}"`);
-    } else {
-      exec(`xdg-open "${url}"`);
-    } */
-  }); 
-  
-  
-/*   myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 10000000);
-	myStatusBarItem.command = myCommandId;
-  subscriptions.push(myStatusBarItem);*/
-  subscriptions.push(disposable);
+	let disposable = vscode.commands.registerCommand('google-search.makeSearch',() => {
+		const activeTextEditor = vscode.window.activeTextEditor;
 
-  //updateStatusBarItem(); 
-  
-  /* const terminal = vscode.window.activeTerminal;
-  if (terminal) {
-    context.subscriptions.push(terminal.onDidChangeSelection((e: { text: any; }) => {
-      const selectedText = e.text;
-      if (selectedText) {
-        vscode.env.clipboard.writeText(selectedText);
-        const encodedText = encodeURIComponent(selectedText);
-        const url = `https://www.google.com/search?q=${encodedText}`;
-        vscode.window.showInformationMessage('Info Notification');
-      }
-    }));
-  } */
+		if (activeTextEditor) {
+			const selectedText = activeTextEditor.document.getText(
+				activeTextEditor.selection
+			);
+
+			vscode.env.openExternal(vscode.Uri.parse(`${URL}${selectedText}`));
+		}
+	});
+
+	let terminalDisposable = vscode.commands.registerCommand('google-search.makeSearchFromTerminal',async () => {
+			const selectedText = await vscode.env.clipboard.readText();
+			vscode.env.openExternal(vscode.Uri.parse(`${URL}${selectedText}`));
+	});
+
+	subscriptions.push(disposable);
+	subscriptions.push(terminalDisposable);
 }
 
 /* function updateStatusBarItem(): void {
